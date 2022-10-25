cd11eafe6c299e4270cc21f145a7dca01e314da9
Marco <marcopg4@gmail.com>
2022-10-25 17:25:43 +0200

    Files and message

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const app = require('express')();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
-const bot = new TelegramBot(process.env['TELEGRAM_TOKEN'], {pooling: true});
-const subscribers = [];
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+const bot = new TelegramBot(process.env['TELEGRAM_TOKEN'], { pooling: true });
+const subscribers = [1096337909];
 
 bot.onText(/\/subscribe/, msg => {
-    const id = msg.chat.id;
-    subscribers.append(id);
-    bot.sendMessage(id, 'Subscription completed');
+    try {
+        const id = msg.chat.id;
+        if (!subscribers.includes(id))
+            subscribers.push(id);
+        bot.sendMessage(id, 'Subscription completed');
+    } catch (e) {
+        console.log(e);
+    }
 });
 
-bot.startPolling();
+bot.onText(/\/unsubscribe/, msg => {
+    try {
+        const id = msg.chat.id;
+        const index = subscribers.indexOf(id);
+        if (index > -1)
+            subscribers.splice(index, 1);
+
+        bot.sendMessage(id, 'Unsubscription completed');
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+bot.setMyCommands([
+    {command: 'subscribe', description: 'Subscribes to notifications'},
+    {command: 'unsubscribe', description: 'Unubscribes from notifications'},
+]);
+
+app.post('/', (req, res) => {
+    if (req?.headers?.token != 'Bearer ' + process.env['SECRET'])
+        res.status(403).send('Authentication failed');
+    subscribers.map(id => {
+        bot.sendPhoto(id, req?.files?.photo?.data);
+        bot.sendMessage(id, req?.body?.text)
+    });
+    res.send('SUCCESS');
+});
+
+app.listen(3000, () => {
+    console.log(`webserver listening localhost:${3000}`);
+    bot.startPolling().then(() => console.log('telegram bot ready'));
+});
